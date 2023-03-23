3749859fa8817f883f4f2b67a54cbaba5e181662
krunal <krunalmiracle@gmail.com>
2023-03-23 17:16:07 +0100

    v0.0.13

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,11 +5,12 @@ import Link from "next/link";
 import { useState, useEffect } from "react";
 
 function Navbar() {
+  const clamp = (min: number, max: number) => (value: number) =>
+    Math.max(Math.min(value, max), min);
   const [show, toggleShow] = useState(false);
   const [clientWindowHeight, setClientWindowHeight] = useState("");
 
-  const [backgroundTransparacy, setBackgroundTransparacy] = useState(0);
-  const [padding, setPadding] = useState(30);
+  /* const [backgroundTransparacy, setBackgroundTransparacy] = useState(1); */
   const [boxShadow, setBoxShadow] = useState(0);
 
   useEffect(() => {
@@ -22,37 +23,34 @@ function Navbar() {
   };
 
   useEffect(() => {
-    let backgroundTransparacyVar = parseFloat(clientWindowHeight) / 600;
-
+    let backgroundTransparacyVar = clamp(
+      0,
+      2
+    )(parseFloat(clientWindowHeight) / 250);
+    console.log("parseFloat" + backgroundTransparacyVar.toString());
     if (backgroundTransparacyVar < 1) {
-      let paddingVar = 30 - backgroundTransparacyVar * 20;
       let boxShadowVar = backgroundTransparacyVar * 0.1;
-      setBackgroundTransparacy(backgroundTransparacyVar);
-      setPadding(paddingVar);
+      /* setBackgroundTransparacy(backgroundTransparacyVar); */
       setBoxShadow(boxShadowVar);
     }
   }, [clientWindowHeight]);
 
   return (
-    <>
-      <div className="absolute top-0 justify-between hidden w-full px-2 bg-gray-300 item-start md:flex md:px-4">
-        <div className="items-center">
-          <p>World&apos;s Favourite Wedding Planning Platform</p>
+    <div className="h-[75px] fixed w-full" style={{
+      boxShadow: `rgb(0 0 0 / ${boxShadow}) 0px 0px 10px 6px`,
+    }}>
+      
+        <div className="items-center justify-between hidden w-full px-2 py-1 bg-white md:flex md:px-4">
+          <div className="">
+            <p>World&apos;s Favourite Wedding Planning Platform</p>
+          </div>
+          <div className="space-x-2 md:flex">
+            <p>Write A Review</p>
+            <p>Download App</p>
+          </div>
         </div>
-        <div className="items-center space-x-2 md:flex">
-          <p>Write A Review</p>
-          <p>Download App</p>
-        </div>
-      </div>
-      <div className="fixed w-full ">
         {/* Website NavBar */}
-        <div
-          className="flex justify-between px-2 h-[75px] md:px-4"
-          style={{
-            background: `rgba(255, 255, 255, ${backgroundTransparacy})`,
-            boxShadow: `rgb(0 0 0 / ${boxShadow}) 0px 0px 20px 6px`,
-          }}
-        >
+        <div className="flex justify-between px-2 h-[75px] md:h-[50px] md:px-4 bg-white">
           <div className="flex-wrap items-center hidden space-x-2 justify-self-start md:flex justify-evenly">
             <p>Venues</p>
             <p>Vendors</p>
@@ -70,17 +68,21 @@ function Navbar() {
             >
               <div className="flex items-center justify-center w-12 h-12 bg-white cursor-pointer group rounded-2xl hover:bg-slate-200">
                 <div className="space-y-1.5">
-                  <span  className={`${
-            show ? "translate-y-1.5 rotate-45 " : ""
-          }block h-1 w-10 origin-center rounded-full bg-slate-500 transition-transform ease-in-out`}></span>
-                  <span className={`${
-            show ? "w-10 -translate-y-1.5 -rotate-45 " : ""
-          }block h-1 w-8 origin-center rounded-full bg-orange-500 transition-transform ease-in-out`} ></span>
+                  <span
+                    className={`${
+                      show ? "translate-y-1.5 rotate-45 " : ""
+                    }block h-1 w-10 origin-center rounded-full bg-slate-500 transition-transform ease-in-out`}
+                  ></span>
+                  <span
+                    className={`${
+                      show ? "w-10 -translate-y-1.5 -rotate-45 " : ""
+                    }block h-1 w-8 origin-center rounded-full bg-orange-500 transition-transform ease-in-out`}
+                  ></span>
                 </div>
               </div>
             </div>
           </button>
-          <div className="w-24 max-w-full justify-self-center">
+          <div className="w-24 max-w-full -mt-3 justify-self-center">
             <Link href="/" className="block w-full navbar-logo">
               <Image src={IconWeb} alt="logo" className="w-full header-logo" />
             </Link>
@@ -152,8 +154,7 @@ function Navbar() {
             </Link>
           </nav>
         </div>
-      </div>
-    </>
+    </div>
   );
 }
 
